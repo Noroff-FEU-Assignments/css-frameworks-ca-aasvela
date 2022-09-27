af18c2a3a58b4b251bddaacb3ead7f54cdcd4675
aasvela <73159941+aasvela@users.noreply.github.com>
2022-09-27 22:12:29 +0200

    Trying to fix layout

--- src/compontents/ContentHome.js
+++ src/compontents/ContentHome.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import ContentJumbo from './ContentJumbo';
 import ContentNavBar from './ContentNavBar';
 import ContentTabs from './ContentTabs';
 
 function Home() {
   return (
-    <Container>
-      <div className="Jumbo"></div>
+    <Container className="body-container">
       <Container>
         <ContentNavBar />
+      </Container>
+      <Container className="body-container">
+        <ContentJumbo />
+      </Container>
+      <Container>
         <h2>We do YAY things</h2>
         <p>
           Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a
@@ -18,8 +23,9 @@ function Home() {
           euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus
           a, varius eget massa.
         </p>
-        <ContentTabs />
       </Container>
+
+      <ContentTabs />
     </Container>
   );
 }
---
+++ src/compontents/ContentJumbo.js
@@ -0,0 +1,18 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap';
+
+function ContentJumbo() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <img fluid src="images/carousel/carousel-1.jpg"></img>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default ContentJumbo;
