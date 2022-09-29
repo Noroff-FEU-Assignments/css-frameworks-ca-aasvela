1cef2ce1a8cbc31033f2469406bc82824eb7005f
aasvela <73159941+aasvela@users.noreply.github.com>
2022-09-29 14:22:56 +0200

    push

--- src/compontents/ContentHome.js
+++ src/compontents/ContentHome.js
@@ -8,24 +8,23 @@ import ContentTabs from './ContentTabs';
 
 function Home() {
   return (
-    <Container className='container-fluid'>
+    <Container className="container-fluid">
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+      <ContentNavBar />
 
-      <ContentNavBar />  
+      <ContentJumbo />
 
-
-        <ContentJumbo />
-
-
-        <h2>We do YAY things</h2>
-        <p>
-          Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a
-          rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend
-          finibus et ac orci. Aliquam metus massa, aliquam quis arcu sit amet,
-          consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa
-          euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus
-          a, varius eget massa.
-        </p>
-
+      <h2>We do YAY things</h2>
+      <p>
+        Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a
+        rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend
+        finibus et ac orci. Aliquam metus massa, aliquam quis arcu sit amet,
+        consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa
+        euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus a,
+        varius eget massa.
+      </p>
 
       <ContentTabs />
     </Container>
@@ -33,8 +32,6 @@ function Home() {
 }
 
 export default Home;
-
-
 
 // function Home() {
 //   return (
--- src/compontents/ContentJumbo.js
+++ src/compontents/ContentJumbo.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap';
+import Jumbotron from  "bootstrap";
 
 function ContentJumbo() {
   return (
@@ -12,6 +13,12 @@ function ContentJumbo() {
         </Col>
       </Row>
     </Container>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Fluid jumbotron</h1>
+      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    </div>
+  </div>
   );
 }
 
