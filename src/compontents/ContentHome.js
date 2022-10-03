f8bb5a6ce849e458dad353bbf5d4e1c48d6b6de9
aasvela <73159941+aasvela@users.noreply.github.com>
2022-10-03 07:58:12 +0200

    added

--- src/compontents/ContentHome.js
+++ src/compontents/ContentHome.js
@@ -8,24 +8,21 @@ import ContentTabs from './ContentTabs';
 
 function Home() {
   return (
-    <Container className="container-fluid">
-      <button type="button" class="btn btn-primary">
-        Primary
-      </button>
+    <Container fluid>
       <ContentNavBar />
 
       <ContentJumbo />
-
-      <h2>We do YAY things</h2>
-      <p>
-        Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a
-        rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend
-        finibus et ac orci. Aliquam metus massa, aliquam quis arcu sit amet,
-        consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa
-        euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus a,
-        varius eget massa.
-      </p>
-
+      <Container>
+        <h2>We do YAY things</h2>
+        <p>
+          Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a
+          rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend
+          finibus et ac orci. Aliquam metus massa, aliquam quis arcu sit amet,
+          consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa
+          euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus
+          a, varius eget massa.
+        </p>
+      </Container>
       <ContentTabs />
     </Container>
   );
@@ -33,30 +30,3 @@ function Home() {
 
 export default Home;
 
-// function Home() {
-//   return (
-//     <Container className="body-container">
-//       <Container>
-//         <ContentNavBar />
-//       </Container>
-//       <Container className="body-container">
-//         <ContentJumbo />
-//       </Container>
-//       <Container>
-//         <h2>We do YAY things</h2>
-//         <p>
-//           Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a
-//           rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend
-//           finibus et ac orci. Aliquam metus massa, aliquam quis arcu sit amet,
-//           consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa
-//           euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus
-//           a, varius eget massa.
-//         </p>
-//       </Container>
-
-//       <ContentTabs />
-//     </Container>
-//   );
-// }
-
-// export default Home;
--- src/compontents/ContentJumbo.js
+++ src/compontents/ContentJumbo.js
@@ -1,24 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap';
-import Jumbotron from  "bootstrap";
+import Image from 'react-bootstrap/Image';
+import Jumbotron from 'bootstrap';
 
 function ContentJumbo() {
   return (
-    <Container className='container-fluid'>
-      <Row>
-        <Col>
-          <img fluid src="images/carousel/carousel-1.jpg"></img>
-        </Col>
-      </Row>
+    <Container fluid>
+      <Image fluid src="images/carousel/carousel-1.jpg"></Image>
     </Container>
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4">Fluid jumbotron</h1>
-      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-    </div>
-  </div>
+    //   <div class="jumbotron jumbotron-fluid">
+    //   <div class="container">
+    //     <h1 class="display-4">Fluid jumbotron</h1>
+    //     <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    //   </div>
+    // </div>
   );
 }
 
