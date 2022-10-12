d9930fd62d71dbf4be8d56915dc99f9a6e146b70
aasvela <73159941+aasvela@users.noreply.github.com>
2022-10-12 21:38:39 +0200

    Update ContentNavBar.js

--- src/compontents/ContentNavBar.js
+++ src/compontents/ContentNavBar.js
@@ -1,32 +1,43 @@
-import Contact from './Contact';
-import News from './News';
-import ContentHome from './ContentHome';
 import { NavLink } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function ContentNavBar() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            {/* <Link to="/">Home</Link> */}
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            {/* <Link to="news">Books</Link> */}
-            <NavLink to="news">News</NavLink>
-          </li>
-          <li>
-            <NavLink to="contact">Contact</NavLink>
-          </li>
-        </ul>
-      </nav>
-
-      {/* <Routes>
-        <Route  path="/" element={<ContentHome />} />
-        <Route path="news" element={<News />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes> */}
+      <Navbar expand="lg">
+        <Container className="nav-container">
+          <Navbar.Brand as={NavLink} to="/">
+            <h1 className="nav-heading">The YAY Company</h1>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto" defaultActiveKey="/home">
+              <Nav.Link as={NavLink} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="news">
+                News
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="contact">
+                Contact
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Go</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
