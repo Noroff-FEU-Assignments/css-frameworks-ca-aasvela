658b590e12089745654d477c6ad368ce960638cb
aasvela <73159941+aasvela@users.noreply.github.com>
2022-10-13 08:29:12 +0200

    news

---
+++ src/compontents/ImageCard.js
@@ -0,0 +1,122 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function ImageCard() {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="images/news/news-1.jpg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="images/news/news-2.jpg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="images/news/news-3.jpg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="images/news/news-4.jpg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="images/news/news-5.jpg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="images/news/news-6.jpg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="images/news/news-7.jpg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="images/news/news-8.jpg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default ImageCard;
--- src/compontents/News.js
+++ src/compontents/News.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import ContentFooter from './ContentFooter';
-
-import ContentJumbo from './ContentJumbo';
-import ContentNavBar from './ContentNavBar';
-import ContentTabs from './ContentTabs';
+import ImageCard from './ImageCard';
 
 function News() {
   return (
     <div className="wrapper">
-      {/* <ContentNavBar /> */}
-      <ContentJumbo />
-      <Container className="home-container">
-        <h2>We do YAY things</h2>
-        <p>
-          Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a
-          rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend
-          finibus et ac orci. Aliquam metus massa, aliquam quis arcu sit amet,
-          consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa
-          euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus
-          a, varius eget massa.
-        </p>
-      </Container>
-      <Container>
-        <ContentTabs />
-      </Container>
-
+      <h2>News</h2>
+      <Container className="home-container"></Container>
+      <ImageCard />
       <ContentFooter />
     </div>
   );
